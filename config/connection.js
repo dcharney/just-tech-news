54b74c785f28034e36f0f17f13d5617b594020da
dcharney-work <delaneycharney@gmail.com>
2021-02-12 03:04:30 +0100

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,12 @@ const config = {
     port: 3306
 };
 
-// create connection to our database, pass in your MySQL information for username and password
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, config);
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, config);
+}
 
 module.exports = sequelize;
